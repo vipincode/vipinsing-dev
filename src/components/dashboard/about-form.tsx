c7c29e4ea64bb5d18cf1d91beb849c8868a52669
Vipin Singh <svipin174@gmail.com>
2024-03-30 23:17:54 +0530

    dashboard navbar layout

--- src/components/dashboard/about-form.tsx
+++ src/components/dashboard/about-form.tsx
@@ -47,91 +47,95 @@ export default function AboutForm() {
   }
 
   return (
-    <div>
-      <Form {...form}>
-        <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
-          <div className="flex justify-between">
-            <h1 className="text-3xl text-primary dark:text-gray-200 mb-6">Add Bio</h1>
-          </div>
-          <div className="grid grid-cols-2 gap-6">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="John Doe" {...field} type="text" disabled={isPending} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="bannerTitle"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Banner Title</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Banner Title" {...field} type="text" disabled={isPending} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="profileTitle"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Profile Title</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Profile Title" {...field} type="text" disabled={isPending} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="designation"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Designation</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Job title" {...field} type="text" disabled={isPending} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormItem>
-              <FormLabel>
-                Profile Image <small className="italic text-muted font-thin">(Optional)</small>
-              </FormLabel>
-              <ImageUpload publicId={publicId} setPublicId={setPublicId} />
-            </FormItem>
-          </div>
+    <Form {...form}>
+      <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex justify-between">
+          <h1 className="text-3xl text-primary dark:text-gray-200 mb-6">Add Bio</h1>
+        </div>
+        <div className="grid grid-cols-2 gap-6">
           <FormField
             control={form.control}
-            name="bio"
+            name="name"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Bio</FormLabel>
+                <FormLabel>Name</FormLabel>
                 <FormControl>
-                  <SimpleMDE placeholder="Description" {...field} />
+                  <Input placeholder="John Doe" {...field} type="text" disabled={isPending} />
                 </FormControl>
-                <FormDescription />
                 <FormMessage />
               </FormItem>
             )}
           />
-          <FormError message={error} />
-          <FormSuccess message={success} />
-          <Button type="submit">{isPending ? 'Submitting...' : '+ Submit'}</Button>
-        </form>
-      </Form>
-    </div>
+          <FormField
+            control={form.control}
+            name="bannerTitle"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Banner Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Banner Title" {...field} type="text" disabled={isPending} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="profileTitle"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Profile Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Profile Title" {...field} type="text" disabled={isPending} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="designation"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Designation</FormLabel>
+                <FormControl>
+                  <Input placeholder="Job title" {...field} type="text" disabled={isPending} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormItem>
+            <FormLabel>
+              Profile Image <small className="italic text-muted font-thin">(Optional)</small>
+            </FormLabel>
+            <ImageUpload publicId={publicId} setPublicId={setPublicId} />
+          </FormItem>
+          <FormItem>
+            <FormLabel>
+              Skills <small className="italic text-muted font-thin">(Optional)</small>
+            </FormLabel>
+            {/* component */}
+          </FormItem>
+        </div>
+        <FormField
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Bio</FormLabel>
+              <FormControl>
+                <SimpleMDE placeholder="Description" {...field} />
+              </FormControl>
+              <FormDescription />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormError message={error} />
+        <FormSuccess message={success} />
+        <Button type="submit">{isPending ? 'Submitting...' : '+ Submit'}</Button>
+      </form>
+    </Form>
   )
 }
--- src/components/dashboard/sidebar-menu.tsx
+++ src/components/dashboard/sidebar-menu.tsx
@@ -10,37 +10,39 @@ import { AdminNavLinks } from './nav-links'
 export default function SidebarMenu() {
   const pathname = usePathname()
   return (
-    <Card className="col-span-2 rounded-none">
-      <div className="flex flex-col justify-between h-full">
-        <div>
-          <div className="h-[59px] px-4 flex items-center border-b border-muted">
-            <span className="text-xs uppercase tracking-wider text-gray-500 dark:text-primary">Dashboard</span>
+    <div className="col-span-2 ">
+      <Card className="rounded-none sticky top-0">
+        <div className="flex flex-col justify-between min-h-screen">
+          <div>
+            <div className="text-center py-2 mt-2">
+              <span className="text-xs uppercase tracking-wider text-gray-500 dark:text-primary">Dashboard</span>
+            </div>
+            <div className="p-4 space-y-6">
+              {AdminNavLinks.map((link) => (
+                <Button
+                  asChild
+                  className="w-full justify-start"
+                  variant={pathname === link.route ? 'default' : 'outline'}
+                  key={link.label}
+                >
+                  <Link href={link.route}>
+                    {link.icon}
+                    {link.label}
+                  </Link>
+                </Button>
+              ))}
+            </div>
           </div>
-          <div className="p-4 space-y-6">
-            {AdminNavLinks.map((link) => (
-              <Button
-                asChild
-                className="w-full justify-start"
-                variant={pathname === link.route ? 'default' : 'outline'}
-                key={link.label}
-              >
-                <Link href={link.route}>
-                  {link.icon}
-                  {link.label}
-                </Link>
+          <div className="pb-4 px-4">
+            <LogoutButton>
+              <Button className="w-full justify-start" variant="ghost">
+                <LogOut size={18} className="mr-2" />
+                Logout
               </Button>
-            ))}
+            </LogoutButton>
           </div>
         </div>
-        <div className="pb-4 px-4">
-          <LogoutButton>
-            <Button className="w-full justify-start" variant="ghost">
-              <LogOut size={18} className="mr-2" />
-              Logout
-            </Button>
-          </LogoutButton>
-        </div>
-      </div>
-    </Card>
+      </Card>
+    </div>
   )
 }
